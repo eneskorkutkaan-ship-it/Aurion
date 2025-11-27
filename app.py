7e458af09b424bd5d3a323d79cacd73b31679193
eneskorkutkaan-ship-it <eneskorkutkaan@gmail.com>
2025-11-27 19:08:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -737,4 +737,4 @@ LOGIN_TEMPLATE = """
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aurion - Giriş</title>
-    <style>""" + BASE_CSS + """ .container { max-width: 400px; margin: 100px auto; padding: 20px; background: #222; border-radius: 10px; } input[type=text], input[type=password] { width: 100%; padding: 10px; margin
+    <style>""" + BASE_CSS + """ .container { max-width: 400px; margin: 100px auto; padding: 20px; background: #222; border-radius: 10px; } input[type=text], input[type=password] { width: 100%; padding: 10px; margin"""
